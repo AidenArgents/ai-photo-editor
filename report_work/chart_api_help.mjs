685f5c3fbf941c88f05c99773ecc5e4cbd088fd1
Aiden Argents <xuyan1993714@gmail.com>
2026-08-31 13:44:19 +0800

    Delete report_work directory

--- report_work/chart_api_help.mjs
+++
@@ -1,8 +0,0 @@
-import { Workbook } from "@oai/artifact-tool";
-
-const workbook = Workbook.create();
-console.log(workbook.help("*", {
-  search: "secondaryAxis|axisGroup|combo|chartType|series",
-  include: "index,examples,notes",
-  maxChars: 6000,
-}).ndjson);
